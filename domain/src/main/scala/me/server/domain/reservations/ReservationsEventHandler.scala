a6c475f3836da3020fac899dbde4e4761b6c3fd6
Pirzmowski <szyborodziuk@gmail.com>
2017-10-29 19:19:39 +0100

    Add repositoryAggregate

--- domain/src/main/scala/me/server/domain/reservations/ReservationsEventHandler.scala
+++ domain/src/main/scala/me/server/domain/reservations/ReservationsEventHandler.scala
@@ -1,17 +1,44 @@
-package me.server.domain.users
+package me.server.domain.reservations
 
-import me.server.domain.users_api.{User, UserCreated, UserUpdated}
+import me.server.domain.reservations_api._
+import me.server.domain.users_api.PersonInfo
 
-object UsersEventHandler {
-  def handleUserCreated(e: UserCreated): User = {
-    User(e.email,e.password,e.firstName,e.lastName,true)
+
+object ReservationsEventHandler {
+  def handleReservationCreated(e: ReservationCreated): Reservation = {
+    Reservation(e.from, e.to, e.clientInfo, e.roomId, 0, None, None, false)
   }
-  def handleUserUpdated(user: User, e: UserUpdated): User = {
-    user.copy(email = e.email.getOrElse(user.email), password = e.password.getOrElse(user.password),
-      firstName = e.firstName.getOrElse(user.firstName), lastName = e.lastName.getOrElse(user.lastName))
+
+  def handleDateChanged(e: DateChanged, reservation: Reservation): Reservation = {
+    reservation.copy(from = e.from.getOrElse(reservation.from), to = e.to.getOrElse(reservation.to))
   }
-  def handleUserDeleted(user: User, e: UserCreated): User = {
-    user.copy(active = false)
+
+  def handleClientInfoChanged(e: ClientInfoChanged, r: Reservation): Reservation = {
+    r.copy(clientInfo = PersonInfo(e.firstName.getOrElse(r.clientInfo.firstName),
+      e.lastName.getOrElse(r.clientInfo.lastName),
+      e.email.getOrElse(r.clientInfo.email),
+      e.phone.getOrElse(r.clientInfo.phone),
+      if(e.personalData.isDefined) e.personalData else r.clientInfo.personalData))
+  }
+
+  def handleRoomChanged(e: RoomChanged, reservation: Reservation): Reservation = {
+    reservation.copy(roomId = e.newRoomId)
+  }
+
+  def handleDiscountChanged(e: DiscountChanged, reservation: Reservation): Reservation = {
+    reservation.copy(discount = e.discount)
+  }
+
+  def handleLoanChanged(e: LoanChanged, reservation: Reservation): Reservation = {
+    reservation.copy(loan = e.loan)
+  }
+
+  def handleReservationDeleted(e: ReservationDeleted, reservation: Reservation): Reservation = {
+    reservation.copy(deleted = true)
+  }
+
+  def handleReservationActived(e: ReservationActived, reservation: Reservation): Reservation = {
+    reservation.copy(deleted = false)
   }
 }
 
